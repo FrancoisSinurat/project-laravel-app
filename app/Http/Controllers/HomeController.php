1e61937feed52001fb11c16e4e12ba46f52eaf43
fajar <fajarqibs@gmail.com>
2024-07-16 19:21:18 +0000

    fix dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,7 +51,7 @@ class HomeController extends Controller
                 'item_categories.item_category_icon as category_icon',
                 'item_categories.item_category_color as category_color',
                 'item_categories.item_category_text as category_text',
-                'item_categories.item_category_color_bg as category_colorBg',
+                'item_categories.item_category_color_bg as category_colorbg',
                 DB::raw('COUNT(assets.item_category_id) as count')
             )
             ->groupBy(
@@ -66,7 +66,7 @@ class HomeController extends Controller
             ->get();
 
             $chartLabel = $cards->pluck('category_name');
-            $chartColor = $cards->pluck('category_colorBg');
+            $chartColor = $cards->pluck('category_colorbg');
             $chartData = $cards->pluck('count');
 
         // Check if the request is AJAX
